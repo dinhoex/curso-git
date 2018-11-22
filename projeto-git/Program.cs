a8f39efc1f12d19c80851c25be20f8049c820c89
Ricardo Watanabe <dinhoex@gmail.com>
2018-11-22 18:23:55 -0200

    Acrescentado a mensagem tenha um bom dia

--- projeto-git/Program.cs
+++ projeto-git/Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace projeto_git {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Ola mundo!");
-            Console.WriteLine("Bom dia");
+            Console.WriteLine("Ola mundo!");            
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um Bom dia");
 
             Console.ReadLine();
         }
