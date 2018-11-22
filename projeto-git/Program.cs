e7ff5d719f567fc5fa5af3d9a24c20c59fbcc749
Ricardo Watanabe <dinhoex@gmail.com>
2018-11-22 15:18:45 -0200

    Projeto iniciado, mostrando a mensagem Ola mundo

---
+++ projeto-git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_git {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ola mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
